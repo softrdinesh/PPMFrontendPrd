f676d467fee435cd5d2277c14c23b10d8847184b
Vhora Abdulrauf <102137649+VhoraAbdulrauf@users.noreply.github.com>
2024-05-21 14:39:52 +0530

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Login />
-      <Register />
+      {/* <Register /> */}
     </>
     // <main
     //   className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
