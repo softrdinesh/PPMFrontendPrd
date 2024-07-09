7daa1dbc777db8b5042c07cbe0b751080cbfcda8
Vhora Abdulrauf <102137649+VhoraAbdulrauf@users.noreply.github.com>
2024-07-09 19:32:29 +0530

    fetch project api integration

--- src/utils/endpoints/project.js
+++ src/utils/endpoints/project.js
@@ -1,6 +1,10 @@
 import { defaults } from './defaults'
 
 export const project = {
+  projectList: {
+    ...defaults.methods.GET,
+    uri: '/api/project'
+  },
   addProject: {
     ...defaults.methods.POST,
     uri: '/api/project'
