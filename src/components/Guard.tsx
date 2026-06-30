cb5cd8bbbb4b21912c328d2b1171e26699f4c4f4
dhamu <dhamukotti31799@gmail.com>
2026-06-30 07:15:30 +0530

    Route fix

--- src/components/Guard.tsx
+++ src/components/Guard.tsx
@@ -41,7 +41,7 @@ const AuthGuard = (props: AuthGuardProps) => {
                         pathname === routes.forgotPassword
                         pathname === routes.verifyEmail
                         pathname === routes.resetPassword
-                        pathname === routes.boards
+                        pathname === routes.boardsview
 
       if (isAuthPage && auth?.user) {
         router.replace(routes.dashboard)
